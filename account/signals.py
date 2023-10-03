c6f646d28d0e3d2c4899cd7c21885682ad48ea0c
Namypark <namy@Namys-MacBook-Pro.local>
2023-10-03 23:22:36 +0100

    Added signal for creating an account when a user is created

--- account/signals.py
+++ account/signals.py
@@ -1,46 +1,27 @@
 from django.db.models.signals import post_save, post_delete
+from django.conf import settings
 from django.dispatch import receiver
 from .models import Account
-from userAuth.models import User
 from django.contrib import messages
 
 
+User = settings.AUTH_USER_MODEL
+
+
 @receiver(post_save, sender=User)
-def create_save_account(sender, instance, created, **kwargs):
+def create_user(sender, instance, created, **kwargs):
     """
     A signal handler that automatically creates an Account when a new User is registered. and saves it if the account is registered or updated
     """
 
-    if created or instance.account is None:
-        Account.objects.create(user=instance)
+    if created:
+        """
+        checks if account just got created
+        then creates a user using the instance(user) that just registered
+        prints a success message
+        """
+        Account.objects.create(user=instance, account_balance=0.00)
         print(instance)
-        print("Account created successfully")
-
-    else:
-        # If the user already has an associated account, save it
+        print("user account created successfully")
         instance.account.save()
-        print(instance)
-        print("Account saved successfully")
-
-
-# # @receiver(post_delete, sender=User)
-# # def delete_user(sender, instance, **kwargs):
-# #     """
-# #     Delete an account from the database and alert the user.
-# #     """
-# #     # Check if the deletion was initiated by an admin user
-# #     if instance != instance.account.user:
-# #         print(instance)
-# #         # Get the associated account
-# #         account = instance.account if hasattr(instance, "account") else None
-
-# #         if account:
-# #             # Delete the account from the database
-# #             account.delete()
-
-# #             # Alert the user
-# #             if instance == instance.account.user:
-# #                 print(instance)
-# #                 messages.info(instance, "Your account has been deleted.")
-# #             else:
-# #                 messages.info(instance, "An admin user has deleted your account.")
+        # messages.success("Account created successfully")
